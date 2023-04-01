ff8afdcc19a4aa9479b563d6c032005840484eaf
Noé <noe.chevassus1@gmail.com>
2023-04-01 19:37:39 +0200

    [DEV] Modification on 2 sql files

--- db_alpha_init.sql
+++ db_alpha_init.sql
@@ -1,3 +1,7 @@
+CREATE USER 'alpha'@'%' IDENTIFIED BY 'password_alpha';
+GRANT ALL PRIVILEGES ON alpha.* TO 'alpha'@'%';
+
 CREATE USER 'beta'@'%' IDENTIFIED BY 'password_beta';
 GRANT ALL PRIVILEGES ON alpha.* TO 'beta'@'%';
+
 FLUSH PRIVILEGES;
--- db_beta_init.sql
+++ db_beta_init.sql
@@ -1,3 +1,7 @@
 CREATE USER 'alpha'@'%' IDENTIFIED BY 'password_alpha';
 GRANT ALL PRIVILEGES ON beta.* TO 'alpha'@'%';
+
+CREATE USER 'beta'@'%' IDENTIFIED BY 'password_beta';
+GRANT ALL PRIVILEGES ON beta.* TO 'beta'@'%';
+
 FLUSH PRIVILEGES;
