ac294edb03e2a837c0b3222fd75ce124e483cbbc
Noé <noe.chevassus1@gmail.com>
2023-04-02 16:55:56 +0200

    [FIX] Corrections on docker-compose file and beta_sql file

--- db_beta_init.sql
+++ db_beta_init.sql
@@ -1,5 +1,5 @@
 
-CREATE USER 'alpha'@'%' IDENTIFIED BY 'password_beta';
+CREATE USER 'alpha'@'%' IDENTIFIED BY 'password_alpha';
 GRANT ALL PRIVILEGES ON beta.* TO 'alpha'@'%';
 
 FLUSH PRIVILEGES;
